cac9a4bc922cea66297e950f8ab8c27a0054d21e
gabriel427131 <gabriel.d.oliveira1@hotmail.com>
2024-01-19 19:38:57 -0700

    First commit
    - Implement project base
    
    - Prepare project pages
    
    - Implement way to display list of meetups

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/View_from_George_Brown_College%27s_Rooftop_Patio_-_panoramio.jpg/1280px-View_from_George_Brown_College%27s_Rooftop_Patio_-_panoramio.jpg",
+    address: " 555 Street, 54321 Toronto",
+    description: "First meetup location!",
+  },
+  {
+    id: "m2",
+    title: "Second meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/View_from_George_Brown_College%27s_Rooftop_Patio_-_panoramio.jpg/1280px-View_from_George_Brown_College%27s_Rooftop_Patio_-_panoramio.jpg",
+    address: " 243 Street, 57891 Toronto",
+    description: "Second meetup location!",
+  },
+];
+
+function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
+
+export default HomePage;
