f474fe1a4def35aa88b005b09562405b8e05cc03
gabriel427131 <gabriel.d.oliveira1@hotmail.com>
2024-01-27 20:11:16 -0700

    Implement API routes
    - Implement MongoDB
    
    - Implement way to send http requests to API routes

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,10 +1,28 @@
-// /api/new-meetup
+import { MongoClient } from "mongodb";
 
-function handler(req, res) {
+// /api/new-meetup
+// POST /api/new-meetup
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    // const { title, image, address, description } = data;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://gabrieldoliveira1:P455WordForMongoDBData@!@cluster0.mkudrv5.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data); // This is one object
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 }
 
